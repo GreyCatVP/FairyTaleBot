c5590f4a7ac2fcd71e6209a0c190a106bfb9ba78
GreyCatVP <a2373761@gmail.com>
2025-06-14 19:15:40 +0300

    🧙‍♂️ Patch 2.3: Тот, что не ломается

--- utils.py
+++ utils.py
@@ -54,27 +54,23 @@ async def generate_fairytale():
         part1_prompt = messages + [{"role": "user", "content": "Напиши первую часть сказки (вступление), до 500 символов."}]
         part1 = await call_openrouter(part1_prompt)
 
-        part2_content = (
-            "Вот первая часть:
-" + part1 + "
+        part2_prompt = messages + [{
+            "role": "user",
+            "content": """Вот первая часть:
+{}
 
-"
-            "Напиши вторую часть сказки (развитие событий), до 500 символов. "
-            "Используй тех же героев и продолжи сюжет."
-        )
-        part2_prompt = messages + [{"role": "user", "content": part2_content}]
+Напиши вторую часть сказки (развитие событий), до 500 символов. Используй тех же героев и продолжи сюжет.""".format(part1)
+        }]
         part2 = await call_openrouter(part2_prompt)
 
-        part3_content = (
-            "Вот первая и вторая части одной сказки:
-" + part1 + "
-" + part2 + "
+        part3_prompt = messages + [{
+            "role": "user",
+            "content": """Вот первая и вторая части одной сказки:
+{}
+{}
 
-"
-            "Заверши именно ЭТУ историю. Не начинай новую! Обязательно сохрани героев и тему. "
-            "Заверши доброй моралью и фразой вроде «Вот и сказке конец»."
-        )
-        part3_prompt = messages + [{"role": "user", "content": part3_content}]
+Заверши именно ЭТУ историю. Не начинай новую! Обязательно сохрани героев и тему. Заверши доброй моралью и фразой вроде «Вот и сказке конец».""".format(part1, part2)
+        }]
         part3 = await call_openrouter(part3_prompt)
 
         story = part1.strip() + "\n\n" + part2.strip() + "\n\n" + part3.strip()
